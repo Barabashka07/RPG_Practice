3eef177353b650b01bb309e4e93934143e7a7a52
Артём Кечкин <115799751+Barabashka07@users.noreply.github.com>
2026-03-31 01:35:13 +0300

    Code ++++

--- Assets/Scripts/Views/Gameplay/PauseView.cs
+++ Assets/Scripts/Views/Gameplay/PauseView.cs
@@ -7,14 +7,17 @@ namespace Views.Gameplay
     //Наследование от View
     public class PauseView : View
     {
+        // Инкапсуляция: кнопки приватные, чтобы никто извне не изменил их визуал
         [SerializeField] private Button saveButton;
         [SerializeField] private Button mainMenuButton;
         [SerializeField] private Button loadLastButton;
         [SerializeField] private Button toLoadChooseButton;
 
         // MVC 
+        // View не знает, КАК сохранять. Он просит Контроллер дать ему Action
         public void SetSaveButtonListener(Action callback)
         {
+            // Вешаем переданную инструкцию на клик кнопки.
             saveButton.onClick.AddListener(callback.Invoke);
         }
         
